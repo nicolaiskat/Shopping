ad7b413554b3ccc18a10ff64dc7a8ab4f2f02c94
Nicolai Skat Nielsen <jegerrockstar@gmail.com>
2021-09-23 15:56:38 +0200

    Initial check-in of module Indkob

---
+++ Indkob/Shared/ShoppingItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Indkob.Shared
+{
+    public class ShoppingItem
+    {
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public bool IsBought { get; set; }
+
+        public int Amount { get; set; }
+    }
+
+}
